3dc9f3efcc399c3d452b49275da6e3d21ab1a925
ibnusyawall <website.isas.24@gmail.com>
2020-10-14 01:36:07 +0700

    🎉 refactoring code

--- routes/api.js
+++ routes/api.js
@@ -1,136 +1,95 @@
-var express = require('express')
-var router  = express.Router()
+const express = require('express')
+const router  = express.Router()
 
-var {
-  Main,
-  Proxy,
-  Headers,
-  UserWP,
-  Base64,
-  Base32,
-  getIp,
-  Gdrive,
-  urlShort,
-  c19,
-  dicoding
-} = require('./../config/utils')
+const {
+    Base
+    Spam
+    MyIP
+    Proxy
+    Hilih
+    Faker
+    Nulis
+    WPUser
+    Corona
+    Headers
+    HashGen
+    Dicoding
+    IpLookup
+    hashIdent
+} = require('./../lib')
 
-var {
-  HashGen
-} = require('./../config/hash')
-
-// var { Movie }    = require('./../config/movie')
-var { FakerGen } = require('./../config/fakeName')
-var { Hilih }    = require('./../config/hilih')
-var { Spam }     = require('./../config/spam_wa')
-var { Check }    = require('./../config/tokped')
 var hashIdent = require('./../config/hash_ident')
 
-// {hash_type, bit_length, char_length, char_type}
-
 router.get('/hash-identifier', (req, res, next) => {
-  const hash = req.query.hash
-  if (!hash) {
-    res.send({
-      code: 403,
-      message: 'Paramater email wajib diisi.'
-    })
-  } else {
-    const main = new hashIdent()
-    main.index(hash, (error, {hash_type, bit_length, char_length, char_type} = {}) => {
-      res.send({
-        hash: hash,
-        hash_type: hash_type,
-        bit_length: bit_length,
-        char_length: char_length,
-        char_type: char_type
-      })
-    })
-  }
+    const hash = req.query.hash
+    if (!hash) {
+        res.send({
+            code: 403,
+            message: 'Paramater email wajib diisi.'
+        })
+    } else {
+        const main = new hashIdent()
+        main.index(hash, (error, {hash_type, bit_length, char_length, char_type} = {}) => {
+            res.send({
+                hash: hash,
+                hash_type: hash_type,
+                bit_length: bit_length,
+                char_length: char_length,
+                char_type: char_type
+            })
+        })
+    }
 })
 
 router.get('/dicoding', (req, res, next) => {
-  const email = req.query.email
-  if (!email) {
-    res.send({
-      code: 403,
-      message: 'Paramater email wajib diisi.'
-    })
-  } else {
-    const main = new dicoding()
-    main._cek(email, (error, {data} = {}) => {
-      res.send({
-        email: email,
-        data
-      })
-    })
-  }
+    const email = req.query.email
+    if (!email) {
+        res.send({
+            code: 403,
+            message: 'Paramater email wajib diisi.'
+        })
+    } else {
+        Dicoding(email)
+            .then(data => {
+                res.send({
+                    email: email,
+                    data
+                })
+            })
+    }
 })
 
 router.get('/corona', (req, res, next) => {
-  const main = new c19()
-  main.indo((error, {negara, positif, sembuh, meninggal, dirawat} = {}) => {
-    res.send({
-      negara,
-      positif,
-      sembuh,
-      meninggal,
-      dirawat
-    })
-  })
+    Corona()
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.send(err)
+        })
 })
 
-router.get('/tokped', (req, res, next) => {
-  const no = req.query.no
-  if (!no) {
-    res.send({
-      code: 200,
-      message: 'Pliese input parameter url.'
-    })
-  } else {
-    const main = new Check()
-    main.tokped(no, (error, {code, message, result} = {} ) => {
-      if (!error) {
-        res.send({
-          code,
-          message,
-          result
-        })
-      } else {
-        res.send({ error })
-      }
-    })
-  }
-})
 
 router.get('/spam', (req, res, next) => {
-  const main = new Spam()
-//  const _key = hash.sha512().update(process.env.KEY_SPAM).digest('hex')
-//  console.log('api key : '+_key)
-  const key   = req.query.key
-  const no    = req.query.no
-  if (!no || !key) {
-    res.send({
-      code: 200,
-      message: 'Pliese input parameter url and api key.'
-    })
-  } else {
-    main.wa(no, key, (error, {code, message, detail} = {} ) => {
-      if (!error) {
+    const key   = req.query.key
+    const no    = req.query.no
+    if (!no || !key) {
         res.send({
-          code,
-          message,
-          detail
+            code: 403,
+            message: 'Input parameter url and api key.'
         })
-      } else {
-        res.send({ error })
-      }
-    })
-  }
+    } else {
+        Spam(no, key)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    }
 })
 
 router.get('/iplookup', (req, res, next) => {
-    const main = new Main()
     const q = req.query.q
     if (!q) {
       res.status(200).send({
@@ -138,147 +97,60 @@ router.get('/iplookup', (req, res, next) => {
         message: 'Pliese input parameter url.'
       })
     } else {
-    main.iplookup(q, (error, {
-      code,
-      message,
-      ip,
-      country,
-      region,
-      latitude,
-      longtitude,
-      timezone,
-      org,
-      as,
-      city,
-      countryCode,
-      zip,
-      maps
-    } = {} ) => {
-      if (!error) {
-        res.send({
-          code,
-          message,
-          ip,
-          country,
-          region,
-          latitude,
-          longtitude,
-          timezone,
-          org,
-          as,
-          city,
-          countryCode,
-          zip,
-          maps
-        })
-      } else {
-        res.status(422).send({ error })
-      }
-    })
+        IpLookup(q)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
-  })
+})
 
-  router.get('/fakename', (req, res, next) => {
-    const main    = new FakerGen()
+router.get('/fakename', (req, res, next) => {
     const country = req.query.country
     if (!country) {
       res.status(500).send({code: 500, message: "Pliese input code country."})
     } else {
-      main.generate(country, (error, {
-        code,
-        message,
-        name,
-        birthday,
-        address,
-        city,
-        country,
-        zip,
-        phone_number,
-        username,
-        password,
-        email
-      } = {} ) => {
-        if (!error) {
-          res.status(200).send({
-            code,
-            message,
-            name,
-            birthday,
-            address,
-            city,
-            country,
-            zip,
-            phone_number,
-            username,
-            password,
-            email
-          })
-        } else {
-            res.status(422).send({ error })
-        }
-      })
+        Faker(country)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(data)
+            })
     }
-  })
+})
 
-  router.get('/proxy', (req, res, next) => {
-    const main = new Proxy()
-    main.generate((error, {
-      code,
-      message,
-      ip,
-      port,
-      protocol,
-      country,
-      allowPost,
-      allowHttps,
-      connectTime,
-      downloadSpeed,
-      uptime
-    } = {} ) => {
-      if (!error) {
-        res.status(200).send({
-          code,
-          message,
-          ip,
-          port,
-          protocol,
-          country,
-          allowPost,
-          allowHttps,
-          connectTime,
-          downloadSpeed,
-          uptime
+router.get('/proxy', (req, res, next) => {
+    Proxy()
+        .then(data => {
+            res.send(data)
         })
-      } else {
-        res.status(500).send({ error })
-      }
-    })
-  })
+        .catch(err => {
+            res.send(err)
+        })
+})
 
-  router.get('/http-headers', (req, res, next) => {
-    const main = new Headers()
+router.get('/http-headers', (req, res, next) => {
     const q = req.query.q
-    console.log(q)
     if (!q) {
       res.status(500).send({
         code: 500,
         message: 'required parameter url.'
       })
     } else {
-      main.generate(q, (error, {result} = {} ) => {
-        if (!error) {
-          res.status(200).send({
-            result
-          })
-        } else {
-          res.status(500).send({ error })
-        }
-      })
+        Headers(q)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
-  })
+})
 
-  router.get('/userwp', (req, res, next) => {
-    const main = new UserWP()
+router.get('/userwp', (req, res, next) => {
     const q = req.query.q
     if (!q) {
       res.status(500).send({
@@ -286,25 +158,17 @@ router.get('/iplookup', (req, res, next) => {
         message: 'Pliese input parameter url.'
       })
     } else {
-      main.user(q, (error, {code, message, urls, user} = {} ) => {
-        if (!error) {
-          res.status(200).send({
-            code,
-            message,
-            urls,
-            user
-          })
-        } else {
-          res.status(500).send({
-            error
-          })
-        }
-      })
+        WPUser(q)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
-  })
+})
 
-  router.get('/base64', (req, res, next) => {
-    const main = new Base64()
+router.get('/base64', (req, res, next) => {
     const encode = req.query.encode
     const decode = req.query.decode
     if (!encode && !decode) {
@@ -313,26 +177,25 @@ router.get('/iplookup', (req, res, next) => {
         message: 'Failed, pliese input parameter.'
       })
     } else if (encode) {
-      main.enc(encode, (error, {code, message, result} = {} ) => {
-        res.status(200).send({
-          code,
-          message,
-          result
-        })
-      })
+        Base('b64enc', encode)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     } else if (decode) {
-      main.dec(decode, (error, {code, message, result} = {} ) => {
-        res.status(200).send({
-          code,
-          message,
-          result
-        })
-      })
+        Base('b64dec', decode)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
-  })
+})
 
-  router.get('/base32', (req, res, next) => {
-    const main = new Base32()
+router.get('/base32', (req, res, next) => {
     const encode = req.query.encode
     const decode = req.query.decode
     if (!encode && !decode) {
@@ -341,75 +204,25 @@ router.get('/iplookup', (req, res, next) => {
         message: 'Failed, pliese input parameter.'
       })
     } else if (encode) {
-      main.enc(encode, (error, {code, message, result} = {} ) => {
-        res.status(200).send({
-          code,
-          message,
-          result
-        })
-      })
+        Base('b32enc', encode)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     } else if (decode) {
-      main.dec(decode, (error, {code, message, result} = {} ) => {
-        res.status(200).send({
-          code,
-          message,
-          result
-        })
-      })
+        Base('b32dec', decode)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
-  })
+})
 
-  router.get('/gdrive', (req, res, next) => {
-    const url = req.query.url
-    const main = new Gdrive()
-    if (!url) {
-      res.send({ code: 400, message: 'pliese enter url'})
-    } else if (!url.match(/drive.google.com/i)) {
-      res.send({ code: 400, message: 'pliese input googledrive only link!.'})
-    } else {
-      main.drive(url, (error, {
-        code,
-        message,
-        result
-      } = {}) => {
-        if (!error) {
-          res.send({
-            code,
-            message,
-            result
-          })
-        } else {
-          res.send({ error })
-        }
-      })
-    }
-  })
-
-  router.get('/ushort', (req, res, next) => {
-    const main = new urlShort()
-    const url  = req.query.url
-    if (!url) {
-      res.status(500).send({
-        code: 500,
-        message: 'pliese input parameter URL!.'
-      })
-    } else {
-      main.short(url, (error, {code, message, result} = {} ) => {
-        if (!error) {
-          res.send({
-            code,
-            message,
-            result
-          })
-        } else {
-          res.send({ error })
-        }
-      })
-    }
-  })
-
-  router.get('/sha1', (req, res, next) => {
-    const main = new HashGen()
+router.get('/sha1', (req, res, next) => {
     const str  = req.query.str
     if (!str) {
       res.send({
@@ -417,18 +230,17 @@ router.get('/iplookup', (req, res, next) => {
         message: 'pliese input parameter url.'
       })
     } else {
-      main.sha1(str, (error, {code, message, result} = {} ) => {
-        res.send({
-          code,
-          message,
-          result
-        })
-      })
+        HashGen('sha1', str)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
-  })
+})
 
-  router.get('/sha256', (req, res, next) => {
-    const main = new HashGen()
+router.get('/sha256', (req, res, next) => {
     const str  = req.query.str
     if (!str) {
       res.send({
@@ -436,18 +248,17 @@ router.get('/iplookup', (req, res, next) => {
         message: 'pliese input parameter url.'
       })
     } else {
-      main.sha256(str, (error, {code, message, result} = {} ) => {
-        res.send({
-          code,
-          message,
-          result
-        })
-      })
+        HashGen('sha256', str)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
-  })
+})
 
-  router.get('/sha512', (req, res, next) => {
-    const main = new HashGen()
+router.get('/sha512', (req, res, next) => {
     const str  = req.query.str
     if (!str) {
       res.send({
@@ -455,61 +266,51 @@ router.get('/iplookup', (req, res, next) => {
         message: 'pliese input parameter url.'
       })
     } else {
-      main.sha512(str, (error, {code, message, result} = {} ) => {
-        res.send({
-          code,
-          message,
-          result
-        })
-      })
+        HashGen('sha512', str)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
-  })
+})
 
-  /* -- disable api
-   router.get('/movie', (req, res, next) => {
-    const main = new Movie()
-    const q = req.query.q
-    if (!q) {
-      res.send({
-        code: 400,
-        message: 'pliese input parameter url.'
-      })
-    } else {
-      main.get(q, (error, {code, message, totalMovie, result} = {} ) => {
-       if (!error) {
-         res.send({
-           code,
-           message,
-           totalMovie,
-           result
-         })
-       } else {
-         res.send({ error })
-       }
-      })
-    }
-  })
 
-  router.get('/cur_ig', (req, res, next) => {
-    res.json({code: 200, message: 'comming soon!.'})
-  }) */
-
-  router.get('/hilih', (req, res, next) => {
+router.get('/hilih', (req, res, next) => {
     const kata = req.query.kata
-    const main = new Hilih()
     if (!kata) {
       res.send({
         code: 400,
-        message: 'pliese input parameter url.'
+        message: 'pliese input parameter kata.'
       })
     } else {
-      main.kintil(kata, (error, {code, result} = {} ) => {
-        res.send({
-          code,
-          result
-        })
-      })
+        Hilih(kata)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
-  })
+})
+
+router.get('/nulis', (req, res, next) => {
+    const kata = req.query.kata
+    if (!kata) {
+      res.send({
+        code: 400,
+        message: 'pliese input parameter kata.'
+      })
+    } else {
+        Nulis(kata)
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    }
+})
 
 module.exports = router
