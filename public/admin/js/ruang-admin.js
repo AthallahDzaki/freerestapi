e14c68d14019ff4224c138f5499d9f0dc701b495
AthallahDzaki <discordgg5@gmail.com>
2020-11-22 15:06:13 +0700

    Bismillah

--- public/admin/js/ruang-admin.js
+++ public/admin/js/ruang-admin.js
@@ -101,11 +101,48 @@ function showTime(){
   //putting time in one variable
   var time = h + ":" + m + ":" + s + " " + session;
   //putting time in our div
-  $('#modalCenter').html(time); 
+  $('#Jam').html(time); 
   //to change time in every seconds
   setTimeout( showTime, 1000 );
 }
 
+startTime();
+
+var dayNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+var newDate = new Date();
+
+var DateString = '';
+for (var i = 0; i < 7; i++) {
+  if (i == newDate.getDay()) {
+    DateString += "<span class='clock__date--curr'>" + dayNames[i] + "</span> ";
+  } else {
+    DateString += "<span>" + dayNames[i] + "</span> ";
+  }
+}
+$('.clock__date').html(DateString);
+
+function startTime() {
+  var today = new Date();
+  var h = checkTime(today.getHours());
+  var m = checkTime(today.getMinutes());
+  var s = checkTime(today.getSeconds());
+
+  var TimeString = h + ':' + m + ':' + s;
+  TimeString = '<span>' + TimeString.split('').join('</span><span>') + '</span>';
+  $('.clock__time').html(TimeString);
+
+  var t = setTimeout(function() {
+    startTime()
+  }, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = '0' + i
+  };
+  return i;
+}
 
 // Popover Javascript
 
