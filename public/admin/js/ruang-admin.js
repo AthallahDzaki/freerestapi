e5ac746dd2fbc6948f7eec610731b05fb8fb5e9f
AthallahDzaki <discordgg5@gmail.com>
2020-11-22 14:05:07 +0700

    Y

--- public/admin/js/ruang-admin.js
+++ public/admin/js/ruang-admin.js
@@ -73,6 +73,43 @@ $(document).ready(function () {
   });
 });
 
+//Jam Digital
+function showTime(){
+  // to get current time/ date.
+  var date = new Date();
+  // to get the current hour
+  var h = date.getHours();
+    // to get the current minutes
+  var m = date.getMinutes();
+  //to get the current second
+  var s = date.getSeconds();
+  // AM, PM setting
+  var session = "AM"; 
+  
+ //conditions for times behavior 
+  if ( h == 0 ) {
+    h = 12;
+  }
+  if( h >= 12 ){
+     session = "PM";
+     }
+  
+  if ( h > 12 ){
+    h = h - 12;
+  }
+  m = ( m < 10 ) ? m = "0" + m : m;
+  s = ( s < 10 ) ? s = "0" + s : s;
+  
+  //putting time in one variable
+  var time = h + ":" + m + ":" + s + " " + session;
+  //putting time in our div
+  $('#Jam').html(time); 
+  //to change time in every seconds
+  setTimeout( showTime, 1000 );
+}
+showTime();
+
+
 // Popover Javascript
 
 $(function () {
