a11aa70e704bab1bd964e093c1ad8fca644a28bc
AthallahDzaki <AthallahDzaki@gmail.com>
2021-07-05 17:09:32 +0700

    Fix Typo USEHEROKU to USING_HEROKU

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var cors = require('cors')
-if(!process.env.USEHEROKU)
+if(!process.env.USING_HEROKU)
 	require('dotenv').config() // Disable Heroku .env for Privacy
 
 var indexRouter = require('./routes/index')
