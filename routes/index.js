d3c29ceb648b814895eff7d5a7b361fc6339a855
AthallahDzaki <AthallahDzaki@gmail.com>
2021-12-15 14:01:15 +0700

    Typo

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 require('dotenv').config()
 
-console.log(prcoess.env);
+console.log(process.env);
 
 const GetHostname = (req) => {
   let header = req.header('x-forwarded-proto');
